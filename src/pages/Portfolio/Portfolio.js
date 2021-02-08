7898cacd3927758efb0822e27a788d27f2a721b9
hoanganh-do <hoangganhh.d0@gmail.com>
2021-02-08 22:56:23 +0700

    format

--- src/pages/Portfolio/Portfolio.js
+++ src/pages/Portfolio/Portfolio.js
@@ -9,3 +9,29 @@ const Portfolio = () => {
 };
 
 export default Portfolio;
+
+
+{/* Skills */}
+{/* <Grid
+container
+spacing={3}
+justify="space-between"
+className="section_graybg pb_45 p_50">
+{resumeData.skills.map((skill) => (
+  <Grid item xs={12} sm={6} md={3}>
+    <Paper className={classes.skill} elevation={0}>
+      <Typography variant="h6" className={classes.skill_title}>
+        {skill.title}
+      </Typography>
+      {skill.description.map((element) => (
+        <Typography variant="body2" className={classes.skill_description}>
+          <TimelineDot variant='outlined' className={classes.timeline_dot} />
+          {element}
+        </Typography>
+      ))}
+
+    </Paper>
+  </Grid>
+))}
+
+</Grid> */}
